170589acc249545b5e3ed53518824fedb1149271
kathechu <162765861+kathechu@users.noreply.github.com>
2024-03-09 16:01:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -465,7 +465,7 @@ st.bar_chart(carbon_df)
 st.caption("Baseline Carbon vs. Final Carbon Total")
 
 st.markdown(f"**Total Carbon Inputs:** {round(carbon_input_tot,3)} tons.")
-st.markdown(f"**Total baseline carbon stock:** {round(base_c, 3)} tons.")
+st.markdown(f"**Total Baseline Carbon Stock:** {round(base_c, 3)} tons.")
 
 st.markdown(f"**Final carbon total** is {round(final_c,3)} tons, a {round(perc_inc, 1)} % increase at location: {point[0]} degrees latitude, {point[1]} degrees longitude.")
 
