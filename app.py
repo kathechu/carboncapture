7544c23db5e331c0236664e19cba100c58942907
kathechu <162765861+kathechu@users.noreply.github.com>
2024-03-08 20:42:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -17,13 +17,7 @@ import streamlit as st
 st.set_page_config(
     page_title="Carbon Capture Cube Calculator",
     page_icon="🌳",
-    layout="wide",
-    initial_sidebar_state="expanded",
-    menu_items={
-        'Get Help': 'https://www.extremelycoolapp.com/help',
-        'Report a bug': "https://www.extremelycoolapp.com/bug",
-        'About': "# This is a header. This is an *extremely* cool app!"
-    }
+    layout="wide"
 )
 
 #import folium
